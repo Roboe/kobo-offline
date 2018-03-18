476929a9c17a5b1b3613a35bb66f624c4a867e3d
Roberto MF <git@virgulilla.com>
2018-03-18 17:58:14 +0100

    PoC call to Kobo API (console)

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 
+import { queryUpdates } from './helpers/koboapi.js'
+
 ReactDOM.render(
   <App />,
   document.getElementById('root'),
 );
+
+queryUpdates('00000000-0000-0000-0000-000000000375', 'kobo')
+  .then((text) => { console.table(text) })
