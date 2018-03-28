96764e81bd1d411f4ea24fe126e1affa9e98ffcc
Roberto MF <git@virgulilla.com>
2018-03-28 03:34:34 +0200

    Rename getCheckUpdateUrl

--- src/helpers/koboapi.js
+++ src/helpers/koboapi.js
@@ -2,10 +2,11 @@ import { AFFILIATES } from '../constants/affiliates.js'
 import corsFetch from './crossorigin.js'
 
 export const checkUpdateEndpoint = (deviceId, affiliate, currentFwVersion = '0.0', serialNumber = 'N') =>
+export const getCheckUpdateUrl = (deviceId, affiliate, currentFwVersion = '0.0', serialNumber = 'N') =>
   `https://api.kobobooks.com/1.0/UpgradeCheck/Device/${deviceId}/${affiliate}/${currentFwVersion}/${serialNumber}`
 
 export function fetchLatestUpdate(deviceId, affiliate) {
-  return corsFetch(checkUpdateEndpoint(deviceId, affiliate))
+  return corsFetch(getCheckUpdateUrl(deviceId, affiliate))
     .then((response) => response.json())
     .then((json) => ({
       data: json.Data,
