56e9e6255b16ed5533442c3dae4326e28fcff1c0
Roberto MF <gitlab@virgulilla.com>
2020-07-30 11:35:49 +0000

    Deploy 399c8dc66c622a853f34fa5588c85ddf2ed76173

---
+++ precache-manifest.1fe2b4a27295ab3e575aa2cb28877a26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "249b878ab06a02c730acfd8be1ed4112",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7d7d9abdfdd3240a8825",
+    "url": "./static/css/main.04acd2be.chunk.css"
+  },
+  {
+    "revision": "3f8e7900bb27395ce183",
+    "url": "./static/js/2.e9a6945a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.e9a6945a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d7d9abdfdd3240a8825",
+    "url": "./static/js/main.0cbee9fd.chunk.js"
+  },
+  {
+    "revision": "d229523dad026dbfe996",
+    "url": "./static/js/runtime-main.2fcf4d27.js"
+  }
+]);
