96548dadc04712af20b04a6431ce7a90ff1c2bdd
Roberto MF <git@virgulilla.com>
2021-06-23 23:47:08 +0200

    Format with Prettier

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
 import '../styles/index.css'
-import '../styles/layout-structure.css';
-import '../styles/general.css';
-import '../styles/code-blocks.css';
-import '../styles/sectioning.css';
-import '../styles/downloads-list.css';
-import '../styles/card.css';
+import '../styles/layout-structure.css'
+import '../styles/general.css'
+import '../styles/code-blocks.css'
+import '../styles/sectioning.css'
+import '../styles/downloads-list.css'
+import '../styles/card.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
