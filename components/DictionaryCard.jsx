d19c66f7461031e7464ab92fb59e8ceee39de264
Roberto MF <git@virgulilla.com>
2021-07-14 21:32:44 +0200

    Next.js v11 doesn't support exporting i18n routes (read more)
    This commit implements a handmade i18n React hook without fancy things that only relies on Next.js's `useRouter` for getting the current locale and default locale.
    
    As a PoC, I made the `DictionaryCard.jsx` presentational component i18n-aware (depending on your strictness, it shouldn't be; it's a PoC, after all). Then I removed/rewrote all the language-related logic from `libs/kobo/dictionaries.js`.
    
    It was working fine. I was happy. Next.js development server worked. And then...
    
    ```
    Error: i18n support is not compatible with next export. See here for more info on deploying: https://nextjs.org/docs/deployment
        at ~/kobo-offline/node_modules/next/dist/export/index.js:14:322
        at async Span.traceAsyncFn (~/kobo-offline/node_modules/next/dist/telemetry/trace/trace.js:6:584)
    npm ERR! code ELIFECYCLE
    npm ERR! errno 1
    npm ERR! kobo-offline@0.1.0 build: `next build && next export`
    npm ERR! Exit status 1
    [...]
    ```
    
    For some unknown and, honestly, unexpected reason, Next.js doesn't want to export the site statically. Even when it knows which routes it should render and in which locale it should render them.
    
    I wasn't expecting it to have server-side locale detection, of course. I just wanted my static pages in two different languages, and I was prepared to [pull the trigger on disabling that server-side feature](https://nextjs.org/docs/advanced-features/i18n-routing#disabling-automatic-locale-detection).
    
    I didn't want client-side i18n from the beginning, so I'm left with these options:
    - changing the framework to something that doesn't even think about server-side execution; maybe react-static. It doesn't have the notion to i18n routes, but I can build routes for each language, anyway. It's not like I have *quite the number* of routes, to be fair. That would open the door to i18nize each route path also, something that Next.js lacks.
    - switching (back?) to Hugo, which is quite fantastic in all what it does (it supports even route paths i18n!), but I'd lose React templating, which is quite unfortunate.
    - mangling around some weird environment-based per-language exporting. Non-english exports in a subpath and managing all those bundles. Boo.

--- components/DictionaryCard.jsx
+++ components/DictionaryCard.jsx
@@ -1,37 +1,61 @@
 import PropTypes from 'prop-types'
-import {
-  getLangName,
-  getDictionaryDownloadUrl,
-} from '../lib/kobo/dictionaries.js'
+import useTranslations from '../lib/i18n/useTranslations.js'
+import { getDictionaryDownloadUrl } from '../lib/kobo/dictionaries.js'
+
+const i18nMessages = {
+  en: {
+    language: {
+      en: 'English',
+      de: 'German',
+      es: 'Spanish',
+      fr: 'French',
+      it: 'Italian',
+      nl: 'Dutch',
+      jp: 'Japanese',
+      pt: 'Portuguese',
+    },
+  },
+  es: {
+    language: {
+      en: 'inglés',
+      de: 'alemán',
+      es: 'español',
+      fr: 'francés',
+      it: 'italiano',
+      nl: 'holandés',
+      jp: 'japonés',
+      pt: 'portugués',
+    },
+  },
+}
 
 const isSingleCodeLang = (langCodeOrCodePair) => langCodeOrCodePair.length === 2
 
-function langCodeOrCodePairToLabel(langCodeOrCodePair, language = 'en') {
-  const NON_BREAKABLE_HYPHEN = '\u2011'
+const NON_BREAKABLE_HYPHEN = '\u2011'
 
-  if (isSingleCodeLang(langCodeOrCodePair)) {
-    return getLangName(language)(langCodeOrCodePair)
-  } else {
-    return langCodeOrCodePair
-      .split('-')
-      .map(getLangName(language))
-      .join(NON_BREAKABLE_HYPHEN)
-  }
+const DictionaryCard = ({ dictionaryLangCodeOrCodePair }) => {
+  const t = useTranslations(i18nMessages)
+
+  const label = isSingleCodeLang(dictionaryLangCodeOrCodePair)
+    ? t.language[dictionaryLangCodeOrCodePair]
+    : dictionaryLangCodeOrCodePair
+        .split('-')
+        .map((lang) => t.language[lang])
+        .join(NON_BREAKABLE_HYPHEN)
+  return (
+    <article
+      className={`card downloads-list--item ${
+        isSingleCodeLang(dictionaryLangCodeOrCodePair)
+          ? 'downloads-list--item_break'
+          : ''
+      }`}
+    >
+      <a href={getDictionaryDownloadUrl(dictionaryLangCodeOrCodePair)}>
+        {label}
+      </a>
+    </article>
+  )
 }
-
-const DictionaryCard = ({ languageCode, dictionaryLangCodeOrCodePair }) => (
-  <article
-    className={`card downloads-list--item ${
-      isSingleCodeLang(dictionaryLangCodeOrCodePair)
-        ? 'downloads-list--item_break'
-        : ''
-    }`}
-  >
-    <a href={getDictionaryDownloadUrl(dictionaryLangCodeOrCodePair)}>
-      {langCodeOrCodePairToLabel(dictionaryLangCodeOrCodePair)}
-    </a>
-  </article>
-)
 DictionaryCard.propTypes = {
   languageCode: PropTypes.string,
   dictionaryLangCodeOrCodePair: PropTypes.string.isRequired,
